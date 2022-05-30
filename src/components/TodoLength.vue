33d73e85d9f2ae91b3224f32ca566036423a2ce3
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-05-31 01:47:18 +0300

    updated

--- src/components/TodoLength.vue
+++ src/components/TodoLength.vue
@@ -5,7 +5,9 @@ defineProps(["todo", "filter", "filters"]);
 <template>
 <div class="span-list">
     <span v-show="todo">{{ todo }} Adet Todo Var.</span>
-    <span v-show="filter.length > 0">{{ filter.length }} Adet Tamamlanmış Todo Var.</span>
+    <div class="active" v-show="filter.length > 0">
+        {{ filter.length }} Adet Tamamlanmış Todo Var.
+    </div>
     <span v-show="filters.length > 0">{{ filters.length }} Adet Tamamlanmamış Todo Var.</span>
 </div>
 </template>
@@ -14,13 +16,23 @@ defineProps(["todo", "filter", "filters"]);
 .span-list {
     display: flex;
     justify-content: space-between;
+
     span {
         display: flex;
         justify-content: flex-start;
-        border: 1px solid black;
-        background: rgb(255, 170, 70);
         padding: 5px 30px;
         border-radius: 5px;
+        color: white;
+        background: #ffaa46;
     }
+}
+
+.active {
+    background: rgb(114, 199, 102);
+    display: flex;
+    justify-content: flex-start;
+    padding: 5px 30px;
+    border-radius: 5px;
+    color: white;
 }
 </style>
